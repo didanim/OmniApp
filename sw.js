1eb3d7d057e2adb672ec8ebabf14bd4b96a1b28b
didanim <173139593+didanim@users.noreply.github.com>
2026-04-17 15:25:56 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,19 @@
 const CACHE_NAME = "omni-v1";
+const OFFLINE_URL = "/"; 
 
 self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([OFFLINE_URL]);
+    })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
